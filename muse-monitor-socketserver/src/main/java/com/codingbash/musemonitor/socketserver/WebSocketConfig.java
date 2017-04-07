3b4d19713aa91c3bbd7ebe062f0b378950c22ea2
CodingBash <bbecerr@outlook.com>
2017-04-07 14:49:26 -0500

    Able to see status on screen!

--- muse-monitor-socketserver/src/main/java/com/codingbash/musemonitor/socketserver/WebSocketConfig.java
+++ muse-monitor-socketserver/src/main/java/com/codingbash/musemonitor/socketserver/WebSocketConfig.java
@@ -6,6 +6,7 @@ import java.util.Queue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
