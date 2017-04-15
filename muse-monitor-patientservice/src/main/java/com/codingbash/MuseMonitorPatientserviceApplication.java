2ebeb322aeaecd92251a67b6ae70ccf43f3c0248
CodingBash <bbecerr@outlook.com>
2017-04-15 18:49:06 -0500

    Added project for rest service

---
+++ muse-monitor-patientservice/src/main/java/com/codingbash/MuseMonitorPatientserviceApplication.java
@@ -0,0 +1,12 @@
+package com.codingbash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseMonitorPatientserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseMonitorPatientserviceApplication.class, args);
+	}
+}
