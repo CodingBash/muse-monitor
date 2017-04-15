6695b84ad10984bd24d8dba471cc1a4182bef13f
CodingBash <bbecerr@outlook.com>
2017-04-15 18:19:47 -0500

    Filtered indicators

--- muse-monitor-socketserver/src/main/java/com/codingbash/musemonitor/socketserver/WebSocketConfig.java
+++ muse-monitor-socketserver/src/main/java/com/codingbash/musemonitor/socketserver/WebSocketConfig.java
@@ -3,7 +3,6 @@ package com.codingbash.musemonitor.socketserver;
 import java.util.Queue;
 
 import org.apache.commons.collections4.queue.CircularFifoQueue;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -13,6 +12,7 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 import com.codingbash.musemonitor.socketserver.model.FallIndicatorWrapper;
 import com.codingbash.musemonitor.socketserver.model.InboundPayload;
+import com.codingbash.musemonitor.socketserver.model.OutboundIndicatorPayload;
 import com.codingbash.musemonitor.socketserver.model.PreviousStatusHolder;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -64,6 +64,12 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 		return new CircularFifoQueue<InboundPayload>(sizeOfCircularQueue);
 	}
 	
+	@Bean
+	public Queue<OutboundIndicatorPayload> indicatorQueue(){
+		int sizeOfCircularQueue = 10;
+		
+		return new CircularFifoQueue<OutboundIndicatorPayload>(sizeOfCircularQueue);
+	}
 	@Bean
 	public FallIndicatorWrapper indicatorWrapper(){
 		FallIndicatorWrapper indicators = new FallIndicatorWrapper();
--- muse-monitor-socketserver/src/main/java/com/codingbash/musemonitor/socketserver/mapper/OutboundIndicatorPayloadMapper.java
+++ muse-monitor-socketserver/src/main/java/com/codingbash/musemonitor/socketserver/mapper/OutboundIndicatorPayloadMapper.java
@@ -1,26 +1,27 @@
 package com.codingbash.musemonitor.socketserver.mapper;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 import com.codingbash.musemonitor.socketserver.model.InboundPayload;
 import com.codingbash.musemonitor.socketserver.model.MentalStatus;
 import com.codingbash.musemonitor.socketserver.model.OutboundIndicatorPayload;
 import com.codingbash.musemonitor.socketserver.model.PhysicalStatus;
-import com.codingbash.musemonitor.socketserver.model.PreviousStatusHolder;
-import com.codingbash.musemonitor.socketserver.processor.FallDeterminationProcessor;
-import com.google.gson.Gson;
 
+// TODO: Modularize status checkers
 @Component
 public class OutboundIndicatorPayloadMapper {
 
 	private final static Logger LOG = LoggerFactory.getLogger(OutboundIndicatorPayloadMapper.class);
 
 	@Autowired
-	private PreviousStatusHolder previousStatus;
+	private Queue<OutboundIndicatorPayload> indicatorQueue;
 
 	public OutboundIndicatorPayload mapOutboundIndicatorPayload(InboundPayload inboundPayload, boolean fallFlag,
 			boolean seizureFlag) {
@@ -29,39 +30,120 @@ public class OutboundIndicatorPayloadMapper {
 		boolean newStatus = false;
 		if (fallFlag) {
 			outboundIndicatorPayload.setPhysicalStatus(PhysicalStatus.EMERGENCY);
-			if (previousStatus.getPhysicalStatus() != PhysicalStatus.EMERGENCY) {
+			if (checkPhysicalStatus(indicatorQueue, inboundPayload, PhysicalStatus.EMERGENCY)) {
 				// New status
 				newStatus = true;
-				previousStatus.setPhysicalStatus(PhysicalStatus.EMERGENCY);
 			}
 		} else {
 			outboundIndicatorPayload.setPhysicalStatus(PhysicalStatus.GOOD);
-			if (previousStatus.getPhysicalStatus() != PhysicalStatus.GOOD) {
+			if (checkPhysicalStatus(indicatorQueue, inboundPayload, PhysicalStatus.GOOD)) {
 				// New status
 				newStatus = true;
-				previousStatus.setPhysicalStatus(PhysicalStatus.GOOD);
 			}
 		}
 
 		if (seizureFlag) {
 			outboundIndicatorPayload.setMentalStatus(MentalStatus.EMERGENCY);
-			if (previousStatus.getMentalStatus() != MentalStatus.EMERGENCY) {
+			if (checkMentalStatus(indicatorQueue, inboundPayload, MentalStatus.EMERGENCY)) {
 				// New status
 				newStatus = true;
-				previousStatus.setMentalStatus(MentalStatus.EMERGENCY);
 			}
 		} else {
 			outboundIndicatorPayload.setMentalStatus(MentalStatus.GOOD);
-			if (previousStatus.getMentalStatus() != MentalStatus.GOOD) {
+			if (checkMentalStatus(indicatorQueue, inboundPayload, MentalStatus.GOOD)) {
 				// New status
 				newStatus = true;
-				previousStatus.setMentalStatus(MentalStatus.GOOD);
 			}
 		}
-
+		System.out.println("NEWSTATUS" + newStatus);
 		if (newStatus) {
 			outboundIndicatorPayload.setTimeMillis(inboundPayload.getTimeMills());
+			indicatorQueue.add(outboundIndicatorPayload);
 		}
 		return outboundIndicatorPayload;
+	}
+
+	private boolean checkPhysicalStatus(Queue<OutboundIndicatorPayload> indicatorQueue, InboundPayload inboundPayload,
+			PhysicalStatus status) {
+		List<OutboundIndicatorPayload> list = new ArrayList<OutboundIndicatorPayload>(indicatorQueue);
+
+		if (!list.isEmpty()) {
+			/*
+			 * Check if target status is different from last status change
+			 */
+			if (list.get(list.size() - 1).getPhysicalStatus() != status) {
+				if (list.size() > 1) {
+					/*
+					 * Find the last status change that is the same as the
+					 * target status
+					 */
+					for (int i = list.size() - 2; i >= 0; i--) {
+						if (list.get(i).getPhysicalStatus() == status) {
+							/*
+							 * If the last synonymous status change was more
+							 * than 3 seconds ago, send indicator change
+							 */
+							if (inboundPayload.getTimeMills() - list.get(i).getTimeMillis() > 3 * 1000) {
+								return true;
+							} else {
+								break;
+							}
+						}
+						return true;
+					}
+				} else {
+					/*
+					 * There is only one saved indicator and it is different
+					 */
+					return true;
+				}
+
+			}
+		} else {
+			return true;
+		}
+		return false;
+	}
+
+	private boolean checkMentalStatus(Queue<OutboundIndicatorPayload> indicatorQueue, InboundPayload inboundPayload,
+			MentalStatus status) {
+		List<OutboundIndicatorPayload> list = new ArrayList<OutboundIndicatorPayload>(indicatorQueue);
+
+		if (!list.isEmpty()) {
+			/*
+			 * Check if target status is different from last status change
+			 */
+			if (list.get(list.size() - 1).getMentalStatus() != status) {
+				if (list.size() > 1) {
+					/*
+					 * Find the last status change that is the same as the
+					 * target status
+					 */
+					for (int i = list.size() - 2; i >= 0; i--) {
+						if (list.get(i).getMentalStatus() == status) {
+							/*
+							 * If the last synonymous status change was more
+							 * than 3 seconds ago, send indicator change
+							 */
+							if (inboundPayload.getTimeMills() - list.get(i).getTimeMillis() > 3 * 1000) {
+								return true;
+							} else {
+								break;
+							}
+						}
+						return true;
+					}
+				} else {
+					/*
+					 * There is only one saved indicator and it is different
+					 */
+					return true;
+				}
+
+			}
+		} else {
+			return true;
+		}
+		return false;
 	}
 }
